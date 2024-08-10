8fcbd33918a7245e1555ffdb3d28ddcb77fb2a0f
beatrizmcbarbosa <biabarbosa98@gmail.com>
2024-08-10 16:18:48 +0100

    Added destinations and display city options based on user continent choice

--- project.py
+++ project.py
@@ -1,6 +1,19 @@
+destinations = {
+    "Europe": ["Lisboa", "Paris", "Madrid"],
+    "Asia": ["Mumbai", "Tokyo"],
+    "Oceania": ["Sydney", "Suva"],
+    "Africa": ["Nairobi", "Abidjan", "Luanda"],
+    "America": ["Lima", "Ciudad do México", "Dallas"],
+}
+
+
 def main():
-    continent = input("In which continent will your holiday be? ").strip().lower()
-    print(continent)
+    continent = input("In which continent will your holiday be? ").strip().capitalize()
+
+    if continent in destinations:
+        print(
+            f"We offer the below options in {continent}:\n{destinations[continent]}\nPlease type the destination where you would like to go"
+        )
 
 
 if __name__ == "__main__":
