72517778b9244df4595158f1e206e7421be56ab6
beatrizmcbarbosa <biabarbosa98@gmail.com>
2024-08-13 21:02:36 +0100

    Added pytest

--- test_project.py
+++ test_project.py
@@ -1,8 +1,10 @@
 from project import duration, holiday, flights, hotels, estimate
+import pytest
 
 
 def test_duration():
     assert duration("2024-08-13", "2024-08-14") == 1
+    assert duration("13 August 2024", "14 August 2024") == 1
 
 
 def test_holiday(): ...
