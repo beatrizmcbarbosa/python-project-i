421ffb34c7a3d5446f940bc39d5ee17e36821ad3
beatrizmcbarbosa <biabarbosa98@gmail.com>
2024-08-13 10:54:32 +0100

    Update main function

--- project.py
+++ project.py
@@ -11,9 +11,13 @@ def main():
     continent = input("In which continent will your holiday be? ").strip().capitalize()
 
     if continent in destinations:
-        print(
-            f"We offer the below options in {continent}:\n{destinations[continent]}\nPlease type the destination where you would like to go"
+        city = input(
+            f"We offer the below options in {continent}:\n{destinations[continent]}\nPlease type the destination where you would like to go: "
         )
+    departure = input(
+        f"We are so happy you want to travel to {city}. When would you like to go? Please answer in the format YYYY-MM-DD: "
+    )
+    arrival = input(f"And come back? (format YYYY-MM-DD)")
 
 
 def duration(): ...
